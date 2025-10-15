62de06a8de95cbf3c35bf68d2dd9f2ea3946bd67
JELmzabi <elmzabijawad@gmail.com>
2025-10-15 23:19:06 +0100

    install inertiaui-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Modal, ModalLink, renderApp } from '@inertiaui/modal-vue'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,9 +17,11 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        return createApp({ render: renderApp(App, props) }) 
             .use(plugin)
             .use(ZiggyVue)
+            .component('ModalLink', ModalLink)
+            .component('Modal', Modal)
             .mount(el);
     },
     progress: {
